b3d23910a75304ee22985f9a4d190a992357369d
Mathew Hultquist <mathew@gates.services>
2022-11-05 12:10:04 -0400

    Added Parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Parallax from './components/Parallax';
 import './styles/main.scss';
+import image from './images/mathew.png';
 
 const App = () => (
     <div className="MainContent">
-        <h1>Test</h1>
+        <Parallax background={image} contentClassName='ParallaxHeader' >
+            <div className="Text">
+                <h1>
+                    Hello,
+                </h1>
+                <h2>
+                    I'm Mathew.
+                </h2>
+            </div>
+        </Parallax>
     </div>
 );
 
--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -5,11 +5,12 @@ import '../styles/components/Parallax.scss';
 const Parallax: FC<{
     width?: string,
     height?: string,
-    background?: string,
+    background?: any,
+    contentClassName?: string,
     children?: any,
-}> = ({ width, height, background, children }) => (
-    <div className="Parallax" style={{ width: width || '100vw', height: height || '100vh', backgroundImage: `url(${background})` || '' }}>
-        <div className="ParallaxContent">
+}> = ({ width, height, background, contentClassName, children }) => (
+    <div className="Parallax" style={{ width: width || '100vw', height: height || '100vh', backgroundImage: `url(${background || ''})` }}>
+        <div className={ `ParallaxContent ${contentClassName || ''}` }>
             { children }
         </div>
     </div>
