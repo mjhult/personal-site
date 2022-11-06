6b3e4a6256d82299bf7ae57b24704853734792a0
Mathew Hultquist <mathew@gates.services>
2022-11-05 22:08:21 -0400

    Added projects

---
+++ src/data/projects.ts
@@ -0,0 +1,10 @@
+import ProjectItem from "../utils/types/ProjectItem";
+
+const projects: ProjectItem[] = [
+    {
+        title: 'Music Browser CLI',
+        desc: 'Have you ever wanted to listen to music but not want to leave your terminal? Well, now you can! This CLI app was made entirely in React as well!',
+    },
+];
+
+export default projects;
