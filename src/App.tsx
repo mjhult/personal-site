7282c374132de85e269f1c027d7ee45358a9cb58
Mathew Hultquist <mathew@gates.services>
2022-11-07 10:19:36 -0500

    Refactor: remove individual containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+
+// Component imports
 import Parallax from './components/Parallax';
-import About from './containers/AboutContainer';
-import Projects from './containers/ProjectContainer';
-import Contact from './containers/ContactContainer';
-import Education from './containers/EducationContainer';
-import Experience from './containers/ExperienceContainer';
-import Skills from './containers/SkillsContainer';
+import Container from './components/Container';
+import AboutItem from './components/AboutItem';
+import SkillItem from './components/SkillItem';
+import ResumeItem from './components/ResumeItem';
+import ProjectItem from './components/ProjectItem';
+import ContactItem from './components/ContactItem';
+
+// Data imports
+import about from './data/about';
+import skills from './data/skills';
+import projects from './data/projects';
+import contacts from './data/contact';
+import educations from './data/education';
+import experiences from './data/experience';
+
+// Image imports
 import image from './images/mathew.png';
-import './styles/resume.scss';
+
+// Style imports
 import './styles/main.scss';
+import './styles/resume.scss';
 
 const App = () => {
     const [ width, setWidth ] = useState(window.innerWidth);
@@ -17,6 +31,8 @@ const App = () => {
     
     return (
         <div className="MainContent">
+
+            {/* Landing parallax */}
             <Parallax background={image} contentClassName='ParallaxHeader' >
                 <div className="Text">
                     <h1>
@@ -27,13 +43,33 @@ const App = () => {
                     </h2>
                 </div>
             </Parallax>
-            <About />
-            <Projects />
+
+            {/* About */}
+            <Container header='About Me' className='AboutContainer'>
+                <div className="AboutItems">
+                    { about.map((item, i) => (
+                        <AboutItem key={i} icon={item.icon} title={item.title} />
+                    )) }
+                </div>
+            </Container>
+
+            {/* Projects */}
+            <Container header='Projects' className='ProjectsContainer'>
+                <div className="ProjectList">
+                    { projects.map((project, i) => (
+                        <ProjectItem key={i} title={project.title} desc={project.desc} image={project.image} imageAlt={project.imageAlt} link={ project.link } />
+                    )) }
+                </div>
+            </Container>
+
+            {/* Resume section header */}
             <div className="ResumeHeader">
                 <h1>
                     My Resume
                 </h1>
             </div>
+
+            {/* Resume header */}
             <div className="Resume">
                 <section className="Header">
                     <div className="HeaderContent">
@@ -46,11 +82,23 @@ const App = () => {
                         </h3>
                     </div>
                 </section>
+
+                {/* Resume Content */}
                 <div className="sideBySide">
 
                     {/* the contact and professional summary */}
                     <div className="right">
-                        <Contact />
+
+                        {/* Contact */}
+                        <Container header='Contact'>
+                            <div className="Contacts">
+                                { contacts.map((contact, i) => (
+                                    <ContactItem key={i} icon={contact.icon} title={contact.title} link={contact.link} />
+                                ))}
+                            </div>
+                        </Container>
+                        
+                        {/* Professional Summary */}
                         <div className="ProfessionalSummary">
                             <h2>
                                 Professional Summary
@@ -62,16 +110,51 @@ const App = () => {
                                 including JavaScript, CSS, HTML, React, and Redux. Able to effectively self-manage during independant projects, 
                                 as well as collaborate in a team setting.</p>
                         </div>
-                        { width > 500 ? <Skills /> : null }
+
+                        {/* Skills (desktop) */}
+                        { width > 500 ? 
+                            <Container header='Skills' className='SkillsContainer'>
+                                <div className="Skills">
+                                    { skills.map((skill, i) => (
+                                        <SkillItem key={i} pointer={skill.pointer} space={skill.space} size={skill.size} skill={skill.skill} />
+                                    ))}
+                                </div>
+                            </Container>
+                        : null }
                     </div>
 
                     {/* main resume content */}
                     <div className="left">
-                        <Experience />
-                        <Education />
-                        { width <= 500 ? <Skills /> : null }
-                    </div>
 
+                        {/* Experience */}
+                        <Container header='Experience' className='ExperienceContainer'>
+                            <div className="Experience">
+                                {experiences.map((experience, i) => (
+                                    <ResumeItem key={i} title={experience.title} subTitle={experience.subTitle} content={experience.content} />
+                                ))}
+                            </div>
+                        </Container>
+
+                        {/* Education */}
+                        <Container header='Education' className='EducationContainer'>
+                            <div className="Education">
+                                { educations.map((education, i) => (
+                                    <ResumeItem key={i} title={education.title} subTitle={education.subTitle} content={education.content} />
+                                ))}
+                            </div>
+                        </Container>
+
+                        {/* Skills (mobile) */}
+                        { width <= 500 ? 
+                            <Container header='Skills' className='SkillsContainer'>
+                                <div className="Skills">
+                                    { skills.map((skill, i) => (
+                                        <SkillItem key={i} pointer={skill.pointer} space={skill.space} size={skill.size} skill={skill.skill} />
+                                    ))}
+                                </div>
+                            </Container>
+                        : null }
+                    </div>
                 </div>
             </div>
         </div>
