3bb8a68cf465fc9e248f5969c95dcd17ad253c89
Mathew Hultquist <mathew@gates.services>
2022-11-07 10:19:49 -0500

    Added github and linkedin

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -3,6 +3,16 @@ import Icons from "../utils/Icons";
 
 const contacts: ContactItem[] = [
     {
+        icon: Icons.gitHub,
+        title: 'Github',
+        link: 'https://github.com/mjhult',
+    },
+    {
+        icon: Icons.linkedIn,
+        title: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/mathew-hultquist/'
+    },
+    {
         icon: Icons.phone,
         title: '(864) 434-3226',
         link: 'tel:8644343226',
