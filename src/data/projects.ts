9471aec9e26b15f14552c48ba6ed304bd0754cf7
Mathew Hultquist <mathew@gates.services>
2022-11-08 19:15:02 -0500

    Changed project description

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ import mcli from '../images/mcli.jpg';
 const projects: ProjectItem[] = [
     {
         title: 'Music Browser CLI',
-        desc: 'Have you ever wanted to listen to music but not want to leave your terminal? Well, now you can! This CLI app was made entirely in React as well!',
+        desc: 'Have you ever wanted to listen to music without leaving the comfort of your terminal? Well now you can! This CLI app was made entirely in React as well!',
         image: mcli,
         imageAlt: 'A music player in your terminal.',
         link: 'https://github.com/WVAviator/music-browser-cli',
